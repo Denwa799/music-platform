022f2f07e64035bb821d4ce5d86db91bf907d0e3
Denwa799 <denwa799@yandex.ru>
2022-06-12 21:36:36 +0300

    Добавляет поиск

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import MainLayout from 'layouts/MainLayout';
 import TrackList from 'components/TrackList';
 import {useRouter} from "next/router";
-import {Grid, Card, Box, Button} from '@mui/material';
+import {Grid, Card, Box, Button, TextField} from '@mui/material';
 import {useTypedSelector} from "hooks/useTypedSelector";
 import {NextThunkDispatch, wrapper} from "store";
-import { fetchTracks } from 'store/actions-creators/track';
+import {fetchTracks, searchTracks} from 'store/actions-creators/track';
+import {useDispatch} from "react-redux";
 
 const Index = () => {
     const router = useRouter();
     const {tracks, tracksError} = useTypedSelector(state => state.track);
+    const [query, setQuery] = useState('');
+    const dispatch = useDispatch() as NextThunkDispatch;
+    const [timer, setTimer] = useState(null);
+
+    const search = async (event: ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+        if (timer) {
+            clearTimeout(timer);
+        }
+        setTimer(
+            setTimeout(async () => {
+                await dispatch(await searchTracks(event.target.value));
+            }, 500)
+        )
+    }
 
     if (tracksError) {
         return (
@@ -30,6 +46,11 @@ const Index = () => {
                                 <Button onClick={() => router.push('/tracks/create')}>Загрузить</Button>
                             </Grid>
                         </Box>
+                        <TextField
+                            fullWidth
+                            value={query}
+                            onChange={search}
+                        />
                         <TrackList tracks={tracks}/>
                     </Card>
                 </Grid>
